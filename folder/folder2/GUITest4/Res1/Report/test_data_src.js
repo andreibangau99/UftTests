5221638b09aa8608642a8b38c13565e90db3377f
abangau <abangau@opentext.com>
2025-01-10 11:35:08 +0200

    folder

---
+++ folder/folder2/GUITest4/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\abangau\\Documents\\UFT One\\GUITest47776\\GUITest4\\Res1',namePairs:[]}]};
